fa253fd46f0fb8a4f2619512f56254ed1a293c31
jingdouping <jingdouping@gmail.com>
2022-11-05 05:24:18 +0900

    “admin後”

--- resources/js/admin/components/sidebar/Sidebar.jsx
+++ resources/js/admin/components/sidebar/Sidebar.jsx
@@ -78,10 +78,6 @@ const Sidebar = () => {
             </li>
         </ul>
       </div>
-      <div className='bottom'>
-        <div className='colorOption' onClick={()=>dispatch({type:'LIGHT'})}></div>
-        <div className='colorOption' onClick={()=>dispatch({type:'DARK'})}></div>
-      </div>
     </div>
   )
 }
